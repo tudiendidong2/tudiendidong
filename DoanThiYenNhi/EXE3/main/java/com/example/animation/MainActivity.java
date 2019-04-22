0ce42d7fabc6efc05e4f818ea693b76f40ca462c
YenNhi1001 <43855647+YenNhi1001@users.noreply.github.com>
2019-04-22 14:56:35 +0700

    Anim

---
+++ DoanThiYenNhi/EXE3/main/java/com/example/animation/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.animation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void clockwise(View view){
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.myanimation);
+        image.startAnimation(animation);
+    }
+    public void zoom(View view){
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.clockwise);
+        image.startAnimation(animation1);
+    }
+}
